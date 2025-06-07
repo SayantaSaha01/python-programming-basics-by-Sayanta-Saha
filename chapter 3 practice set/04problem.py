c82064a0720f37c3c18bcd3ecd8d36a8ff366d7c
Sayanta Saha <iamsayantasaha01@gmail.com>
2025-06-07 18:43:10 +0530

    add

---
+++ chapter 3 practice set/04problem.py
@@ -0,0 +1,9 @@
+#3. Write a program to detect double space in a string.
+
+a="sayanta is  a boss"
+print(a.replace("  "," "))  #.replace is a string built in function
+print(a) #strings are immutable,you can not change the permenant data by using function  ex: here it remain same after using func..
+
+# output
+# sayanta is a boss
+# sayanta is  a boss
